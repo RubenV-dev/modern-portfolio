ded33103743e03b1153624613094539c2bf6f061
Ruben Vallejo <r.will16@hotmail.com>
2025-06-12 14:02:00 -0400

    working on about page

--- src/components/about.js
+++ src/components/about.js
@@ -7,11 +7,27 @@ export default class About extends React.Component {
         <>
         <Nav/>
         <div className="about">
-            <p className="description">Full Stack Web Developer with experience creating, maintaining, and testing software deployments at scale. Skilled in React, 
-            JavaScript, Ruby/Rails, SQL, Java, Spring Boot, Docker, Kubernetes, and deploying applications to AWS EKS, Azure AKS with ADD, and Google GKE. Active open 
-            source contributor with an interest in feature creation and optimization. With 3 years experience at a large tech company, I bring strong analytical skills 
-            to technical projects with a compelling desire to build products and features that create an impact.
+            <p className="description">
+            Full Stack Web Developer with 3 years experience creating and testing product features and deployments.
+            I also have years of experience in the healthcare/research sector and bring strong analytical 
+            skills to technical projects along with strong customer support experience obtained from working 
+            directly with patients.
             </p>
+            <p>
+            Open Source contributor to 
+            <a href="https://github.com/backstage/backstage"
+            target="_blank"
+            rel="noopener noreferrer">
+            Backstage OSS.
+            </a>
+            </p>
+
+            <li>Experienced developing software products in Javascript, Typescript, Java and Ruby.</li>
+
+            <li>Experienced with frameworks: Rails, React, SpringBoot.</li>
+
+            <li>Skilled with using the following tools: Containerization using Docker, Kubernetes, and cloud 
+            application deployments using AWS ECS and EKS, Azure AKS with ADD, and Google GKE.</li> 
         </div>
         </>
     )
