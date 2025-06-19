1bffd0d6a6dc4df6c0907191b55a27f948b826e1
Ruben Vallejo <r.will16@hotmail.com>
2025-06-19 11:38:23 -0400

    style change to about page

--- src/components/about.js
+++ src/components/about.js
@@ -8,26 +8,30 @@ export default class About extends React.Component {
         <Nav/>
         <div className="about">
             <p className="description">
-            Full Stack Web Developer with 3 years experience creating and testing product features and deployments.
-            I also have years of experience in the healthcare/research sector and bring strong analytical 
-            skills to technical projects along with strong customer support experience obtained from working 
-            directly with patients.
+              Full Stack Web Developer with 3 years experience creating and testing product features and deployments.
+              I also have years of experience in the healthcare/research sector and bring strong analytical 
+              skills to technical projects along with strong customer support experience obtained from working 
+              directly with patients.
             </p>
+
             <p>
-            Open Source contributor to 
-            <a href="https://github.com/backstage/backstage"
-            target="_blank"
-            rel="noopener noreferrer">
-            Backstage OSS.
-            </a>
+              Open source contributor to &nbsp;
+              <a href="https://github.com/backstage/backstage"
+              target="_blank"
+              rel="noopener noreferrer">
+              Backstage OSS.
+              </a>
             </p>
 
-            <li>Experienced developing software products in Javascript, Typescript, Java and Ruby.</li>
+            <ul>
+              <li>Experienced Languages: Javascript, Typescript, Java and Ruby.</li>
 
-            <li>Experienced with frameworks: Rails, React, SpringBoot.</li>
+              <li>Experienced Frameworks: Rails, React, SpringBoot.</li>
 
-            <li>Skilled with using the following tools: Containerization using Docker, Kubernetes, and cloud 
-            application deployments using AWS ECS and EKS, Azure AKS with ADD, and Google GKE.</li> 
+              <li>Experienced Tools: Docker, Kubernetes, and cloud deployments using AWS ECS and EKS, 
+                Azure AKS with AD, and Google GKE.</li> 
+            </ul>
+
         </div>
         </>
     )
