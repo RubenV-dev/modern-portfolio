7f71b847ddc7f002408c48f1b1ccc5d17e5f8cc1
Ruben Vallejo <r.will16@hotmail.com>
2025-06-29 14:47:51 -0400

    fix nav bar bug on smaller view

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Iframe from 'react-iframe'
 import Nav from './nav'
 
 export default class Projects extends React.Component {
@@ -12,7 +11,7 @@ export default class Projects extends React.Component {
             <div className="proj-card">
                 <h3>Pinniped Auth Provider</h3>
                 <p>Authentication Supervisor Integration with backstage</p>
-                <p>Built with: <span class="yellow-text">Typescript, React</span></p>
+                <p>Built with: <span className="yellow-text">Typescript, React</span></p>
                 <a href="https://github.com/backstage/backstage/pull/19846"
                     target="_blank"
                     rel="noopener noreferrer"> 
@@ -21,31 +20,31 @@ export default class Projects extends React.Component {
             <div className="proj-card">
                 <h3>Metadata cache</h3>
                 <p>Add a metadata cache for the Pinniped Authentication Provider</p>
-                <p>Built with: <span class="yellow-text">Typescript, React</span></p>
+                <p>Built with: <span className="yellow-text">Typescript, React</span></p>
                 <a href="https://github.com/backstage/backstage/pull/20909" target="_blank" rel="noopener noreferrer"> Merged PR</a>
             </div>
             <div className="proj-card">
                 <h3>Seperate OIDC module migration</h3>
                 <p>Migrate OIDC authentication provider to seperate module for auth-backend plugin in backstage</p>
-                <p>Built with: <span class="yellow-text">Typescript, React</span></p>
+                <p>Built with: <span className="yellow-text">Typescript, React</span></p>
                 <a href="https://github.com/backstage/backstage/pull/20282" target="_blank" rel="noopener noreferrer"> Merged PR</a>
             </div>
             <div className="proj-card">
                 <h3>Enable Server Side Authentication</h3>
                 <p>Utilized Kubernetes Auth Proxy on backstage to allow for generalized request handling</p>
-                <p>Built with: <span class="yellow-text">Typescript, React</span></p>
+                <p>Built with: <span className="yellow-text">Typescript, React</span></p>
                 <a href="https://github.com/backstage/backstage/pull/16649" target="_blank" rel="noopener noreferrer"> Merged PR</a>
             </div>
             <div className="proj-card">
                 <h3>Kubernetes-backend proxy client plugin</h3>
                 <p>Add a reusable kubernetes-backend proxy client to allow plugins to make generalized requests</p>
-                <p>Built with: <span class="yellow-text">Typescript, React</span></p>
+                <p>Built with: <span className="yellow-text">Typescript, React</span></p>
                 <a href="https://github.com/backstage/backstage/pull/17121" target="_blank" rel="noopener noreferrer"> Merged PR</a>
             </div>
             <div className="proj-card">
                 <h3>PermissionPolicy toggle for Kubernetes Proxy</h3>
                 <p>Add a permission policy toggle for proxy endpoint to disable use</p>
-                <p>Built with: <span class="yellow-text">Typescript, React</span></p>
+                <p>Built with: <span className="yellow-text">Typescript, React</span></p>
                 <a href="https://github.com/backstage/backstage/pull/16237" target="_blank" rel="noopener noreferrer"> Merged PR</a>
             </div>
             
@@ -61,7 +60,7 @@ export default class Projects extends React.Component {
                         foods they ate that day. Users will then be able to see total caloric, protein, carb and fat intake from the meals entered.
                         Useful to keep track of this info when you are trying to reach your fitness goals.
                     </p>
-                    <p>Built with: <span class="yellow-text">React, Redux, Hooks, React-vis, Ruby on Rails</span>.</p>
+                    <p>Built with: <span className="yellow-text">React, Redux, Hooks, React-vis, Ruby on Rails</span>.</p>
 
                     <a href="https://github.com/RubenV-dev/intake-front-end" target="_blank" rel="noopener noreferrer">
                         Front-End
@@ -82,7 +81,7 @@ export default class Projects extends React.Component {
                 to the high score list. Play to have the highest score.
                 </p>
                 <p>
-                    Built with: <span class="yellow-text">Javascript, Ruby on Rails, HTML Canvas</span>.
+                    Built with: <span className="yellow-text">Javascript, Ruby on Rails, HTML Canvas</span>.
                 </p>
                 <a href="https://github.com/RubenV-dev/k9-stroller-js-app" target="_blank" rel="noopener noreferrer">
                     Front - End
