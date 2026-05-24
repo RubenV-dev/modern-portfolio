91dc88f6c0290ee3e3b6a404813fbffe04dbcf8e
Ruben Vallejo <r.will16@hotmail.com>
2026-05-23 21:17:08 -0400

    fix: remove white bar on top and bottom on mobile

--- src/mainContainer.js
+++ src/mainContainer.js
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export default class MainContainer extends React.Component {
   render() {
     return (
-      <div className="main-container text-white w-full h-full bg-[url('https://i.imgur.com/dbpOZ8z.jpeg')] bg-cover bg-center bg-no-repeat">
+      <div className="text-white w-full h-full min-h-[100dvh] bg-[url('https://i.imgur.com/dbpOZ8z.jpeg')] bg-cover bg-center bg-no-repeat">
         <Outlet />
       </div>
     );
